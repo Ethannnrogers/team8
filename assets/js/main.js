344bcec52b7df007058c08ac9aa578a6ef41c982
Ethan <erogers999@gmail.com>
2020-01-05 16:24:02 -0500

    big update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(function($) {
 
+	$('button').on('click', function(event) {
+		var target = $(this.getAttribute('href'));
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').stop().animate({
+				scrollTop: target.offset().top
+			}, 80);
+		}
+	});
+
+
+
+
 	"use strict";
 
 	var loader = function() {
